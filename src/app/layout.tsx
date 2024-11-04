0b946c0ed2d01b2f156cd54f27551fc76b79b9f3
rohitk131 <rohitkumar13103@gmail.com>
2024-11-04 22:56:02 +0530

    adding metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,43 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Mockly",
-  description: "Mockly lets you generate mockup for iPhone X/XS/Max, iPad, MacBook and other devices quickly. ",
+  title: "Mockly - Generate Device Mockups Quickly",
+  description: "Create beautiful mockups for devices like iPhone X/XS/Max, iPad, MacBook, Lumina, and more in seconds with Mockly. Perfect for designers and developers.",
+  keywords: [
+    "mockups",
+    "device mockups",
+    "iPhone mockup generator",
+    "iPad mockups",
+    "MacBook mockups",
+    "Lumina UI mockups",
+    "Mockly",
+    "design tools",
+    "UI mockups",
+    "web development tools",
+  ],
+  authors: [{ name: "Rohit Kumar" }],
+  openGraph: {
+    title: "Mockly - The Ultimate Device Mockup Generator",
+    description: "Effortlessly create professional device mockups for iPhone, iPad, MacBook, Lumina, and more with Mockly.",
+    url: "https://mocklyapp.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "./og-image.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Mockly - Generate Device Mockups Quickly",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mockly - Generate Device Mockups Quickly",
+    description: "Instantly create device mockups for iPhone, iPad, MacBook, Lumina, and more. Perfect for designers and developers.",
+    images: ["./og-image.jpeg"],
+  },
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -25,6 +60,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="canonical" href="https://mockly.com" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
